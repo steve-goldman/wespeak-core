b72e315fd8ce8f0f2bd5f1aea949b3891f533d53
Steve Goldman <steve@wespeaktogether.com>
2015-02-17 00:08:42 -0500

    rename

--- java/test/com/wespeak/core/engine/EngineTest.java
+++ java/test/com/wespeak/core/engine/EngineTest.java
@@ -1,6 +1,5 @@
 package com.wespeak.core.engine;
 
-import com.wespeak.core.EventPublisher;
 import com.wespeak.core.SpecialStatements;
 import com.wespeak.core.StatementState;
 import com.wespeak.core.Vote;
@@ -15,30 +14,29 @@ import org.junit.Test;
 import java.io.File;
 import java.io.IOException;
 
-public class EngineTestBase
+public class EngineTest
 {
-    private static final boolean   DELETE_TMP_FILE_ON_EXIT = false;
+    private static final boolean DELETE_TMP_FILE_ON_EXIT = false;
 
-    protected static final long    T0          = 398781000;
-    protected static final long    OneMinute   = 1000 * 60;
-    protected static final long    OneHour     = 60 * OneMinute;
-    protected static final long    OneDay      = 24 * OneHour;
+    private static final long    T0          = 398781000;
+    private static final long    OneMinute   = 1000 * 60;
+    private static final long    OneHour     = 60 * OneMinute;
+    private static final long    OneDay      = 24 * OneHour;
 
-    protected static final String  Steve  = "steve";
+    private static final String  Steve  = "steve";
 
-    protected DataManager          dataManager;
-    protected File                 outputFile;
-    protected TextDumpEventWriter  writer;
-    protected GroupParameters      groupParameters;
+    private DataManager          dataManager;
+    private TextDumpEventWriter  writer;
+    private GroupParameters      groupParameters;
 
-    protected Engine               engine;
+    private Engine               engine;
 
     @Before
     public void setup() throws IOException
     {
         dataManager     = DataManagerFactory.getInstance();
 
-        outputFile      = File.createTempFile("engineTest", null, new File("/tmp"));
+        final File outputFile = File.createTempFile("engineTest", null, new File("/tmp"));
         if (DELETE_TMP_FILE_ON_EXIT)
         {
             outputFile.deleteOnExit();
@@ -93,9 +91,7 @@ public class EngineTestBase
                                    final int  voteThreshold,
                                    final int  yesThreshold)
     {
-        final String setup = "setup";
-
-        engine.heartbeat(0, setup);
+        engine.heartbeat(0, Steve);
 
         for (final String text : new String[] {
                 SpecialStatements.makeUserTTL(userTTL),
@@ -105,7 +101,7 @@ public class EngineTestBase
                 SpecialStatements.makeVoteThreshold(voteThreshold),
                 SpecialStatements.makeYesThreshold(yesThreshold) })
         {
-            primeParameter(setup, text);
+            primeParameter(Steve, text);
         }
 
         engine.pulse(1);
@@ -131,98 +127,4 @@ public class EngineTestBase
         Assert.assertEquals(Vote.YES, dataManager.getVote(userId, statementId));
     }
 
-    protected class DebugEventPublisher implements EventPublisher
-    {
-        private final EventPublisher eventPublisher;
-
-        private boolean didHeartbeat;
-        private boolean didSubmit;
-        private boolean didSupport;
-        private boolean didVote;
-        private boolean didTimeoutUser;
-        private boolean didTimeoutStatement;
-        private boolean didBeginVote;
-        private boolean didEndVoteAccepted;
-        private boolean didEndVoteRejected;
-
-        DebugEventPublisher(final EventPublisher eventPublisher)
-        {
-            this.eventPublisher = eventPublisher;
-        }
-
-
-
-        @Override
-        public void heartbeat(final long now, final String userId, final long userActiveUntil)
-        {
-
-        }
-
-        @Override
-        public void submit(final long   now,
-                           final String userId,
-                           final String statementId,
-                           final String text,
-                           final long   statementActiveUntil,
-                           final int    numEligibleSupporters,
-                           final int    propSupportNeeded,
-                           final long   userActiveUntil)
-        {
-
-        }
-
-        @Override
-        public void support(final long   now,
-                            final String userId,
-                            final String statementId,
-                            final long   userActiveUntil)
-        {
-
-        }
-
-        @Override
-        public void vote(final long   now,
-                         final String userId,
-                         final String statementId,
-                         final Vote   vote,
-                         final long   userActiveUntil)
-        {
-
-        }
-
-        @Override
-        public void timeoutUser(final long now, final String userId)
-        {
-
-        }
-
-        @Override
-        public void timeoutStatement(final long now, final String statementId)
-        {
-
-        }
-
-        @Override
-        public void beginVote(final long   now,
-                              final String statementId,
-                              final long   until,
-                              final int    numEligibleVoters,
-                              final int    propVotesNeeded,
-                              final int    propYesesNeeded)
-        {
-
-        }
-
-        @Override
-        public void endVoteAccepted(final long now, final String statementId)
-        {
-
-        }
-
-        @Override
-        public void endVoteRejected(final long now, final String statementId)
-        {
-
-        }
-    }
 }
